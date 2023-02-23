cf36f2b14ff5f998d28be8f96abaeb31f443ea9b
Kanithkar Kumar <kanithkar@xerago.com>
2023-02-23 09:49:08 +0530

    New change

--- src/components/ChildPerson.js
+++ src/components/ChildPerson.js
@@ -4,7 +4,7 @@ import React from 'react'
 function ChildPerson({person}) {
   return (
     <div>
-       <h2>My name {person.name} My age is {person.age} and my skil is {person.skill}</h2>
+       <h2>My naeeeme {person.name} My age is {person.age} and my skil is {person.skill}</h2>
     </div>
   )
 }
